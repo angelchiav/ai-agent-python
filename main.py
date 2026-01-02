a24f6cf33ef476ae0eeee46ba297408b93f2e8f5
angelchiav <achiavicuna@gmail.com>
2026-01-02 14:15:13 -0300

    first files and working agent

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import argparse
+
+load_dotenv()
+
+parser = argparse.ArgumentParser(description="chatbot")
+parser.add_argument("user_prompt", type=str, help="user prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
+args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("GEMINI_API_KEY not set")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=messages
+)
+
+if args.verbose:
+    print(f'User prompt: {args.user_prompt}\n')
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(f'Response:\n{response.text}')
+
+else:
+    print(f'Response:\n{response.text}')
