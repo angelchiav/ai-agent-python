c6ac8c84b8387240b0d0629820f2f99b315830bc
angelchiav <achiavicuna@gmail.com>
2026-01-03 09:45:59 -0300

    new schemas, handlers and main responses

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
 
 def get_file_content(working_directory: str, file_path: str) -> str:
     try:
@@ -22,3 +23,18 @@ def get_file_content(working_directory: str, file_path: str) -> str:
         
     except Exception as e:
         return f"Error: {e}"
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the contents of a file (up to a maximum) relative to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read, relative to the working directory",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
+from call_function import available_functions, call_function
 
 load_dotenv()
 
@@ -12,6 +13,12 @@ parser.add_argument("user_prompt", type=str, help="user prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 
 args = parser.parse_args()
+
+config = types.GenerateContentConfig(
+    tools=[available_functions], 
+    system_instruction=system_prompt,
+)
+
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,16 +28,21 @@ if api_key == None:
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash', 
+    model='gemini-2.5-flash',
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=config,
 )
 
-if args.verbose:
-    print(f'User prompt: {args.user_prompt}\n')
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
-    print(f'Response:\n{response.text}')
+function_calls = getattr(response, "function_calls", None)
+if function_calls:
+    for function_call in function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
-else:
-    print(f'Response:\n{response.text}')
+if response.text:
+    if args.verbose:
+        print(f'User prompt: {args.user_prompt}\n')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\n')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}\n')
+        print(f'Response:\n{response.text}')
+    else:
+        print(f'Response:\n{response.text}')
